17a817841dd2beded1af0345a3ad7a1cbcd38233
PhilippLenders <philipplenders@gmail.com>
2018-10-18 13:04:15 +0200

    lib created

---
+++ projects/ng6-cdk-lib/src/lib/ng6-cdk-lib.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ng6-cdk-lib',
+  template: `
+    <p>
+      ng6-cdk-lib works!
+    </p>
+  `,
+  styles: []
+})
+export class Ng6CdkLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
